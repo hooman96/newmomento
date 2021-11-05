b704d8dd938d2c02362c4b83c317f01db162300d
hooman96 <hooman.mohammadi1996@gmail.com>
2021-11-05 18:53:55 -0400

    new momento

---
+++ src/js/email.js
@@ -0,0 +1,13 @@
+(function () {
+  const win = window
+  const doc = document.documentElement
+
+  // doc.classList.remove('no-js')
+  // doc.classList.add('js')
+
+  var input = document.getElementById('email').value;
+
+  document.write(5)
+
+  }
+}())
